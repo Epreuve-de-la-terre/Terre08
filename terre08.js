39db81ce85bf2d93a5a8fdf941f3dfef8594323b
Matheos Romain <romain.matheos.31@gmail.com>
2022-01-09 09:26:10 +0100

    Script qui affiche la taille de l'argument

---
+++ terre08.js
@@ -0,0 +1,15 @@
+#! node
+
+const arg = process.argv
+const arg2 = process.argv[2]
+const num = arg2 - 1
+
+console.log(arg.length)
+
+if (arg.length !== 3) {
+  console.log("Veuillez indiquer au script, qu'un seul argument")
+} else if (!isNaN(num)){
+  console.log("Veuillez indiquer au script, un string en argument. Ex : node terre08.js \"Le ciel est bleu\"")
+} else {
+  console.log(arg2.length)
+}
